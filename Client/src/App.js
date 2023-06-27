4fb85e81100e5b1c04e9a2a8d76b50ea9dc7fb05
unknown <parmarsinghbhanu@gmail.com>
2023-06-27 21:38:35 +0530

    partner added

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './Universal/Header';
 import Footer from './Universal/Footer';
+import Partner from './Pages/Ourpartners/Partner';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Footer/>
+      <Partner/>
     </div>
   );
 }
