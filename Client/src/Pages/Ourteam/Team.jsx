198437ed61c69fa146fb33aa5f88f418120c22f0
unknown <parmarsinghbhanu@gmail.com>
2023-06-30 18:22:52 +0530

    contact us don

---
+++ Client/src/Pages/Ourteam/Team.jsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+
+// const Team = () => {
+//   return (
+//     <div>Team
+// <div className="container">
+//     <div className="row">
+//         <div className="col-md-4 col-sm-6">
+//             <div className="our-team">
+//                 <img src="./Images/house-1.jpg" alt="" />
+//                 <h3 className="title">Peter Johns</h3>
+//                 <span className="post">Web</span>
+//             </div>
+//         </div>
+
+//         <div className="col-md-4 col-sm-6">
+//             <div className="our-team">
+//             <img src="./Images/house-1.jpg" alt="" />
+//                 <h3 className="title">Kelli Smith</h3>
+//                 <span className="post">Web Designer</span>
+//             </div>
+//         </div>
+
+//         <div className="col-md-4 col-sm-6">
+//             <div className="our-team">
+//                 <img src="./Images/house-1.jpg" alt="" />
+//                 <h3 className="title">Peter Johns</h3>
+//                 <span className="post">Web Developer</span>
+//             </div>
+//         </div>
+//     </div>
+// </div> 
+//     </div>
+//   )
+// }
+
+// export default Team
