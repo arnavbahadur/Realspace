2236f215197b6fbf0ae1bea11c3bdbf9d7fd3e6b
Aman <amansahu5051@gmail.com>
2023-07-01 13:07:15 +0530

    compare page

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,12 +5,14 @@ import Header from './Universal/Header';
 // import Footer from './Universal/Footer';
 import HouseBox from './Components/HouseBox/HouseBox';
 import HousePreview from './Pages/HousePreview/HousePreview';
+import Compare from './Pages/Compare/Compare';
 function App() {
   return (
     <div className="App">
       <Header/>
       <HouseBox/>
       <HousePreview/>
+      <Compare/>
       {/* <Footer/> */}
     </div>
   );
--- Client/src/Pages/HousePreview/HousePreview.jsx
+++ Client/src/Pages/HousePreview/HousePreview.jsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 import './HousePreview.css'
 const HousePreview = () => {
   const [liked,setliked] = useState(false);
+  const features = ["swimming pool","gym","Tenis court","Elivator"];
+  const featuresElem = features.map((item)=>{
+    return(
+      <li>{item}</li>
+    )
 
+  })
   return (
     <div className='housePreivew-page'> 
       <div className="housePreview-topSection">
@@ -39,11 +45,11 @@ const HousePreview = () => {
               <p className="housePreview-propertyid">
                 <span style={{fontWeight:'bold'}}>Property Id :</span>
                 <span>{`112-23-34`}</span>
-                <i class="fa-solid fa-copy"/>
+                <i className="fa-solid fa-copy"/>
               </p>
             </div>
             <div className='housePreview-discription-top-right'>
-              <i class="fa-solid fa-share"/>
+              <i className="fa-solid fa-share"/>
               <i className="fa-solid fa-heart" onClick={()=>setliked(!liked)} style={{color:`${liked?"red":"#aaadb1"}`}}/>
               <i className="fa-solid fa-arrows-turn-to-dots"/>
             </div>
@@ -74,7 +80,7 @@ const HousePreview = () => {
             <div>
               <p>Year</p>
               <div>
-                <i class="fa-solid fa-calendar-days"></i>
+                <i className="fa-solid fa-calendar-days"></i>
                 <span>{`2014`}</span>
               </div>              
             </div>
@@ -91,11 +97,27 @@ const HousePreview = () => {
               djkfldjg
             </p>
           </div>
-          <div className="housediscription-textArea">
-            <h3>Common Note</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt commodi labore unde! Fuga porro maiores, consequatur id laudantium quod repudiandae vel saepe cum doloribus reiciendis molestiae, atque voluptate rem laboriosam mollitia dicta placeat dolorem ex, voluptatibus fugiat maxime non. Doloribus iusto possimus placeat at voluptatibus error nisi laboriosam repellat odit.
-              djkfldjg
-            </p>
+          <div className="housediscription-features">
+            <h3>Features</h3>
+            <ul>
+              {featuresElem}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="housePreview-section-1 housePreview-section">
+        <div className='housePreview-videoSection housePreview-iframe-section'>
+          <h3>Property video</h3>
+          <div className="housePreview-video housePreview-iframeContainer">
+            {/* <img src="https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="house" />
+            <div><p>Video Tour</p></div> */}
+            <iframe src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen/>
+          </div>
+        </div>
+        <div className='housePreview-mapSection housePreview-iframe-section'>
+          <h3>Map</h3>
+          <div className="housePreview-map housePreview-iframeContainer">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117852.10298046564!2d75.65939514335936!3d22.62102239999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962f958dcb7169d%3A0xd877c12078e50f0f!2sMedi-Caps%20University!5e0!3m2!1sen!2sin!4v1688136971443!5m2!1sen!2sin" width="800" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/>
           </div>
         </div>
       </div>
