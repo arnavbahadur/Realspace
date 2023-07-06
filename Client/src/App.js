baacbfa33e3680f0e81c2cf91fa7b924b811d85f
unknown <parmarsinghbhanu@gmail.com>
2023-07-06 15:10:59 +0530

    simplificatin in app.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,17 +3,13 @@ import './App.css';
 import { Route, Routes} from "react-router-dom";
 import Header from './Universal/Header';
 import Footer from './Universal/Footer';
-import HouseBox from './Components/HouseBox/HouseBox';
+// import HouseBox from './Components/HouseBox/HouseBox';
 import HousePreview from './Pages/HousePreview/HousePreview';
 import Compare from './Pages/Compare/Compare';
-// import Partner from './Pages/Ourpartners/Partner';
-// import Carousel from './Pages/Home/Carousel';
 import Contact from './Pages/Contact/Contact';
 import Faq from './Components/Faq/Faq';
-import Team from './Pages/About/Ourteam/Team';
 import Home from './Pages/Home/Home';
 import Projectbox from './Components/Projectbox/Projectbox';
-import Filter from './Pages/Home/Filter.jsx';
 import Appointment from './Pages/Contact/Appointment/Appointment';
 import CompareSideBtn from './Components/CompareSideBtn/CompareSideBtn'
 import MyModal from './Pages/Home/MyModal';
@@ -31,15 +27,13 @@ function App() {
             <Route path="/compare" element={<Compare/>}/>
             <Route path="/project" element={<Project/>}/>
             <Route path="/about" element={<About/>}/>
-            <Route path="/houseBox" element={<HouseBox/>}/>
+            {/* <Route path="/houseBox" element={<HouseBox/>}/> */}
             <Route path="/housePreview" element={<HousePreview/>}/>
             <Route path="/faq" element={<Faq/>}/>
             <Route path="/projectbox" element={<Projectbox/>}/>
-            <Route path="/filter" element={<Filter/>}/>
             <Route path="/appointment" element={<Appointment/>}/>
             <Route path="/mymodal" element={<MyModal/>}/>     
-        </Routes>
-        
+        </Routes> 
         <Footer/>  
     </div>
   );
