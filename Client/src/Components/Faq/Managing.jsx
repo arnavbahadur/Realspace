37a2c627e11523a018b91f0ec60050040bf5fbfe
shyamptdr0 <anu.shyam.patidar@gmail.com>
2023-07-08 13:26:37 +0530

    project

--- Client/src/Components/Faq/Managing.jsx
+++ Client/src/Components/Faq/Managing.jsx
@@ -3,13 +3,11 @@ import Singlefaq from './Singlefaq';
 const Managing = () => {
   return (
     <div>
-        <div className="managing">Managing your Account
         <div className="accordion" id="accordionExample">
         <Singlefaq headingOne={"headingOne"} collapseOne={"collapseOne"} title={"First title"} content={"First Content"}/>
         <Singlefaq headingOne={"headingTwo"} collapseOne={"collapseTwo"} title={"Second title"} content={"Second Content"}/>
         <Singlefaq headingOne={"headingThird"} collapseOne={"collapseThird"} title={"Third title"} content={"Third Content"}/>
         </div>
-    </div>
     </div>
   )
 }
