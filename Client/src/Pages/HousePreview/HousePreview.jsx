1414210c2f114636087a00964bc9e66a92d16dcc
unknown <parmarsinghbhanu@gmail.com>
2023-07-21 01:08:20 +0530

    integration of property card

--- Client/src/Pages/HousePreview/HousePreview.jsx
+++ Client/src/Pages/HousePreview/HousePreview.jsx
@@ -1,14 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './HousePreview.css'
 import CompareSideBtn from '../../Components/CompareSideBtn/CompareSideBtn';
 import Comparebtn from '../../Components/Comparebtn/Comparebtn';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
+import axios from 'axios';
+import Gallery from '../Gallery/Gallery';
 
 const HousePreview = (props) => {
+
+  let { id } = useParams();
+  const [content,setContent]=useState([]);
+  const propertyapi = async () => {
+    await axios.get(`/propertyapi/${id}`).then((property) => {
+      setContent(property.data);
+      console.log(property.data)
+    });
+  };   
+
+
+useEffect(() => {
+propertyapi();
+}, []);
+
+
   const location = useLocation()
   console.log("location",location)  //location.state brings data passed in navigate hook
   console.log("location",location.state?.id)  //location.state brings data passed in navigate hook
-  const id = location.state?.id;
+
+
+  // const id = location.state?.id;          ye line bhanu me comment k he 
+
+
+
   // location.state.setOk ( true);
   console.log("location.state.ok",location.state?.ok)  //location.state brings data passed in navigate hook
   
@@ -18,13 +41,13 @@ const HousePreview = (props) => {
     const [warned,setWarned] = useState(false);
 
   // console.log('location.state.id ', location.state.id)
-  const features = ["swimming pool","gym","Tenis court","Elivator"];
-  const featuresElem = features.map((item)=>{
-    return(
-      <li>{item}</li>
-    )
+  // const features = ["swimming pool","gym","Tenis court","Elivator"];
+  // const featuresElem = features.map((item)=>{
+  //   return(
+  //     <li>{item}</li>
+  //   )
 
-  })
+  // })
 
   return (
     <div className='housePreivew-page'> 
@@ -42,13 +65,13 @@ const HousePreview = (props) => {
               {`Name in Place`}
             </p>
             <p className="housePreview-location-name">
-              {`Location of place`}
+              {content.location}
             </p>
           </div>      
           <div className="housePreview-top-right">
             <p className="housePreview-price">
-              <span>{`For sale/rent`}</span>
-              <span className='housePreview-price-amount'>{`$20,000`}</span>
+              <span>{content.Purpose}</span>
+              <span className='housePreview-price-amount'>{content.price}</span>
             </p>
           </div>          
         </div>        
@@ -62,7 +85,7 @@ const HousePreview = (props) => {
             <div className='housePreview-discription-top-left'>
               <p className="housePreview-propertyid">
                 <span style={{fontWeight:'bold'}}>Property Id :</span>
-                <span>{`112-23-34`}</span>
+                <span>{content._id}</span>
                 <i className="fa-solid fa-copy"/>
               </p>
             </div>
@@ -93,48 +116,43 @@ const HousePreview = (props) => {
               <p>Bedrooms</p>
               <div>
                 <i className="fa-solid fa-bed"/>
-                <span>{`no`}</span>
+                <span>{content.bedRoom}</span>
               </div>              
             </div>
             <div>
               <p>Bathrooms</p>
               <div>
                 <i className="fa-solid fa-shower"/>
-                <span>{`no`}</span>
+                <span>{content.bathRoom}</span>
               </div>              
             </div>
             <div>
               <p>Area</p>
               <div>
                 <i className="fa-regular fa-square"/>
-                <span>{`no`}</span>
+                <span>{content.areaSqFt}</span>
               </div>              
             </div>
             <div>
               <p>Year</p>
               <div>
                 <i className="fa-solid fa-calendar-days"></i>
-                <span>{`2014`}</span>
+                <span>{content.created_at}</span>
               </div>              
             </div>
           </div>
           <div className="housediscription-textArea">
             <h3>Discription</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt commodi labore unde! Fuga porro maiores, consequatur id laudantium quod repudiandae vel saepe cum doloribus reiciendis molestiae, atque voluptate rem laboriosam mollitia dicta placeat dolorem ex, voluptatibus fugiat maxime non. Doloribus iusto possimus placeat at voluptatibus error nisi laboriosam repellat odit.
-              djkfldjg
-            </p>
+            <p>{content.Description} </p>
           </div>
           <div className="housediscription-textArea">
             <h3>Additional details</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt commodi labore unde! Fuga porro maiores, consequatur id laudantium quod repudiandae vel saepe cum doloribus reiciendis molestiae, atque voluptate rem laboriosam mollitia dicta placeat dolorem ex, voluptatibus fugiat maxime non. Doloribus iusto possimus placeat at voluptatibus error nisi laboriosam repellat odit.
-              djkfldjg
+            <p>(content.Nearby.place)
             </p>
           </div>
           <div className="housediscription-features">
             <h3>Features</h3>
-            <ul>
-              {featuresElem}
-            </ul>
+            <ul> {content.Feature}</ul>  
           </div>
         </div>
       </div>
@@ -144,16 +162,18 @@ const HousePreview = (props) => {
           <div className="housePreview-video housePreview-iframeContainer">
             {/* <img src="https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="house" />
             <div><p>Video Tour</p></div> */}
-            <iframe src="https://www.youtube.com/embed/9ALM5gPYhJA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen/>
+            <iframe src={content.property_url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen/>
           </div>
         </div>
         <div className='housePreview-mapSection housePreview-iframe-section'>
           <h3>Map</h3>
           <div className="housePreview-map housePreview-iframeContainer">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117852.10298046564!2d75.65939514335936!3d22.62102239999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962f958dcb7169d%3A0xd877c12078e50f0f!2sMedi-Caps%20University!5e0!3m2!1sen!2sin!4v1688136971443!5m2!1sen!2sin" width="800" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/>
+            <iframe src={content.location_url} width="800" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/>
           </div>
         </div>
+        {/* <Gallery gallery={content.Gallery}/> */}
       </div>
+      
     </div>
   )
 }
