f19ac7bf54a6bed3aaa2323fbf061a3816794763
shyamptdr0 <anu.shyam.patidar@gmail.com>
2023-07-24 13:24:23 +0530

    about-choose

---
+++ Client/src/Pages/About/Ourteam/PopupChoose.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PopupChoose = (props) => {
+  return (
+    <div className="popup-bg">
+    <div className='popup-class' id="popup1">
+      <div className="popup-inner">
+        <h3 className='bttn-close' onClick={()=>{props.setPopup(false)}}> X close </h3>
+      <h1>{props.popupcontain.title}</h1>
+      <p>{props.popupcontain.content}</p>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default PopupChoose
