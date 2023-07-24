87db02c123fa4dcc162c2d59e5d0e3c27bb31abc
shyamptdr0 <anu.shyam.patidar@gmail.com>
2023-07-24 22:46:26 +0530

    gallery modification

---
+++ Client/src/Pages/Gallery/GalleryBunch.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Gallery from './Gallery'
+import "./Gallery.css";
+const GalleryBunch = () => {
+  return (
+    <div className='gallerybunch'>
+        <Gallery/>
+        <Gallery/>
+        <Gallery/>
+    </div>
+  )
+}
+
+export default GalleryBunch
