15b3a94839ea7491c6124f480d4dff29cd5167b2
unknown <parmarsinghbhanu@gmail.com>
2023-08-10 15:45:35 +0530

    filter me change

--- Models/Property.js
+++ Models/Property.js
@@ -131,3 +131,5 @@ module.exports = Property;
     //         }
     //     }
     // ],
+
+    // <iframe width="560" height="315" src="https://www.youtube.com/embed/9ALM5gPYhJA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
