0e780fec11d0dfd2a7e711e9f35de45ede4b3043
unknown <parmarsinghbhanu@gmail.com>
2023-09-08 20:22:00 +0530

    position arranged

--- Client/src/Admin/Pages/Property/AddProperty.jsx
+++ Client/src/Admin/Pages/Property/AddProperty.jsx
@@ -143,159 +143,23 @@ const AddProperty = () => {
       <div className="add-area add_event_area" id="add_event_area">
         <p>Add new Property</p>
         <div>
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="Title"
-            onChange={handleChange}
-            name="title"
-            value={FormData.title}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="location"
-            onChange={handleChange}
-            name="location"
-            value={FormData.location}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="price"
-            onChange={handleChange}
-            name="price"
-            value={FormData.price}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="area"
-            onChange={handleChange}
-            name="areaSqFt"
-            value={FormData.areaSqFt}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="propertytype"
-            onChange={handleChange}
-            name="propertytype"
-            value={FormData.propertytype}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="balcony"
-            onChange={handleChange}
-            name="balcony"
-            value={FormData.balcony}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="totalfloor"
-            onChange={handleChange}
-            name="totalfloor"
-            value={FormData.totalfloor}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="constructionstage"
-            onChange={handleChange}
-            name="constructionstage"
-            value={FormData.constructionstage}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="dateofpossesion"
-            onChange={handleChange}
-            name="dateofpossesion"
-            value={FormData.dateofpossesion}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="created_at"
-            onChange={handleChange}
-            name="created_at"
-            value={FormData.created_at}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="property_url of ifram"
-            onChange={handleChange}
-            name="property_url"
-            value={FormData.property_url}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="location_url of ifram"
-            onChange={handleChange}
-            name="location_url"
-            value={FormData.location_url}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="Purpose"
-            onChange={handleChange}
-            name="Purpose"
-            value={FormData.Purpose}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="bedRoom"
-            onChange={handleChange}
-            name="bedRoom"
-            value={FormData.bedRoom}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="hall"
-            onChange={handleChange}
-            name="hall"
-            value={FormData.hall}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="bathRoom"
-            onChange={handleChange}
-            name="bathRoom"
-            value={FormData.bathRoom}
-          />
-          <input
-            type="text"
-            id=""
-            className="eventtitle"
-            placeholder="CurrentStatus"
-            onChange={handleChange}
-            name="CurrentStatus"
-            value={FormData.CurrentStatus}
-          />
+          <input type="text" id="" className="eventtitle"  placeholder="Title"  onChange={handleChange}   name="title"  value={FormData.title} />
+          <input type="text" id="" className="eventtitle"  placeholder="location"  onChange={handleChange}   name="location"  value={FormData.location} />
+          <input type="text" id="" className="eventtitle"  placeholder="price"  onChange={handleChange}   name="price"  value={FormData.price} />
+          <input type="text" id="" className="eventtitle"  placeholder="area"  onChange={handleChange}   name="areaSqFt"  value={FormData.areaSqFt} />
+          <input type="text" id="" className="eventtitle"  placeholder="propertytype"  onChange={handleChange}   name="propertytype"  value={FormData.propertytype} />
+          <input type="text" id="" className="eventtitle"  placeholder="balcony"  onChange={handleChange}   name="balcony"  value={FormData.balcony} />
+          <input type="text" id="" className="eventtitle"  placeholder="totalfloor"  onChange={handleChange}   name="totalfloor"  value={FormData.totalfloor} />
+          <input type="text" id="" className="eventtitle"  placeholder="constructionstage"  onChange={handleChange}   name="constructionstage"  value={FormData.constructionstage} />
+          <input type="text" id="" className="eventtitle"  placeholder="dateofpossesion"  onChange={handleChange}   name="dateofpossesion"  value={FormData.dateofpossesion} />
+          <input type="text" id="" className="eventtitle"  placeholder="created_at"  onChange={handleChange}   name="created_at"  value={FormData.created_at} />
+          <input type="text" id="" className="eventtitle"  placeholder="property_url of ifram"  onChange={handleChange}   name="property_url"  value={FormData.property_url} />
+          <input type="text" id="" className="eventtitle"  placeholder="location_url of ifram"  onChange={handleChange}   name="location_url"  value={FormData.location_url} />
+          <input type="text" id="" className="eventtitle"  placeholder="Purpose"  onChange={handleChange}   name="Purpose"  value={FormData.Purpose} />
+          <input type="text" id="" className="eventtitle"  placeholder="bedRoom"  onChange={handleChange}   name="bedRoom"  value={FormData.bedRoom} />
+          <input type="text" id="" className="eventtitle"  placeholder="hall"  onChange={handleChange}   name="hall"  value={FormData.hall} />
+          <input type="text" id="" className="eventtitle"  placeholder="bathRoom"  onChange={handleChange}   name="bathRoom"  value={FormData.bathRoom} />
+          <input type="text" id="" className="eventtitle"  placeholder="CurrentStatus"  onChange={handleChange}   name="CurrentStatus"  value={FormData.CurrentStatus} />
           <textarea
             id=""
             cols="10"
